30b7d1cc08efa4d798ef524046b4f557f06276ed
github username <github email address>
2019-01-26 12:58:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'string';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
